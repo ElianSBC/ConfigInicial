cdad7ff0280588a6b4cbe08fda61eeeb0bf26bf1
elian <elian@ELIANSBC>
2026-09-25 04:17:10 -0600

    practica 6 primera parte

--- ConfigInicial/Carga de Modelos.cpp
+++ ConfigInicial/Carga de Modelos.cpp
@@ -45,63 +45,71 @@ GLfloat lastFrame = 0.0f;
 
 
 
-int main( )
+
+int main()
 {
     // Init GLFW
-    glfwInit( );
+    glfwInit();
     // Set all the required options for GLFW
-    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
-    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
-    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
-    glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
-    glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
-    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
     // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "Barqueras Capultitla Elian Serge", nullptr, nullptr );
-    
-    if ( nullptr == window )
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Carga de modelos y camara sintetica", nullptr, nullptr);
+
+    if (nullptr == window)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate( );
-        
+        glfwTerminate();
+
         return EXIT_FAILURE;
     }
-    
-    glfwMakeContextCurrent( window );
-    
-    glfwGetFramebufferSize( window, &SCREEN_WIDTH, &SCREEN_HEIGHT );
-    
+
+    glfwMakeContextCurrent(window);
+
+    glfwGetFramebufferSize(window, &SCREEN_WIDTH, &SCREEN_HEIGHT);
+
     // Set the required callback functions
-    glfwSetKeyCallback( window, KeyCallback );
-    glfwSetCursorPosCallback( window, MouseCallback );
-    
+    glfwSetKeyCallback(window, KeyCallback);
+    glfwSetCursorPosCallback(window, MouseCallback);
+
     // GLFW Options
     //glfwSetInputMode( window, GLFW_CURSOR, GLFW_CURSOR_DISABLED );
-    
+
     // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
     glewExperimental = GL_TRUE;
     // Initialize GLEW to setup the OpenGL Function pointers
-    if ( GLEW_OK != glewInit( ) )
+    if (GLEW_OK != glewInit())
     {
         std::cout << "Failed to initialize GLEW" << std::endl;
         return EXIT_FAILURE;
     }
-    
+
     // Define the viewport dimensions
-    glViewport( 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT );
-    
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
     // OpenGL options
-    glEnable( GL_DEPTH_TEST );
-    
+    glEnable(GL_DEPTH_TEST);
+
     // Setup and compile our shaders
-    Shader shader( "Shader/modelLoading.vs", "Shader/modelLoading.frag" );
-    
-    // Load models
+    Shader shader("Shader/modelLoading.vs", "Shader/modelLoading.frag");
+
+    // Load models---------------------------------------------
+
+
+
+    Model futbol((char*)"Models/10536_soccerball_V1_iterations-2.obj");
+    Model maleta((char*)"Models/suitcase_obj.obj");
     Model dog((char*)"Models/RedDog.obj");
-    Model calculadora((char*)"Models/calculadora.obj");
-    glm::mat4 projection = glm::perspective( camera.GetZoom( ), ( float )SCREEN_WIDTH/( float )SCREEN_HEIGHT, 0.1f, 100.0f );
-    
-  
+    Model pelota((char*)"Models/13517_Beach_Ball_v2_L3.obj");
+
+
+    glm::mat4 projection = glm::perspective(camera.GetZoom(), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+
+
 
     // Game loop
     while (!glfwWindowShouldClose(window))
@@ -126,19 +134,44 @@ int main( )
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
         // Draw the loaded model
-        glm::mat4 model(1);
-        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));}
-
+        glm::mat4 modelDog(1.0f); // Matriz identidad
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelDog));
 
         dog.Draw(shader);
 
-        calculadora.Draw(shader);
+
+       
+        glm::mat4 modelPelota(1.0f); 
+        modelPelota = glm::translate(modelPelota, glm::vec3(-0.85f, -0.27f, 0.0f));
+        modelPelota = glm::scale(modelPelota, glm::vec3(0.01f, 0.01f, 0.01f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelPelota));
+        pelota.Draw(shader);
+
+
+        glm::mat4 modelmaleta(1.0f);
+        modelmaleta = glm::translate(modelmaleta, glm::vec3(-0.40f, -0.37f, 0.0f));
+        modelmaleta = glm::rotate(modelmaleta, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        modelmaleta = glm::scale(modelmaleta, glm::vec3(0.01f, 0.01f, 0.01f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelmaleta));
+
+        maleta.Draw(shader);
+
+        glm::mat4 modelfutbol(1.0f);
+        modelfutbol = glm::translate(modelfutbol, glm::vec3(-0.4f, 0.0f, 0.0f));
+        modelfutbol = glm::rotate(modelfutbol, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        modelfutbol = glm::scale(modelfutbol, glm::vec3(0.01f, 0.01f, 0.01f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelfutbol));
+
+        futbol.Draw(shader);
+
+        
+        
 
         // Swap the buffers
-        glfwSwapBuffers( window );
+        glfwSwapBuffers(window);
     }
-    
-    glfwTerminate( );
+
+    glfwTerminate();
     return 0;
 }
 
